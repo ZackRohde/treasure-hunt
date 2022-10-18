24804dd1f689b73bd2508e5ca72a9e321a930bb3
ZackRohde <zdrohde@outlook.com>
2022-10-18 16:16:08 -0700

    fixed sizing's for mobile view

---
+++ treasure-hunt/src/App.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react"
+import './App.css'
+import Square from './components/square.js'
+
+const App = () => {
+  const [board, setBoard] = useState([
+    "?",
+    "?",
+    "?",
+    "?",
+    "?",
+    "?",
+    "?",
+    "?",
+    "?"
+  ])
+
+  const [treasureLocation, setTreasureLocation] = useState(Math.floor(Math.random() * board.length))
+  const [bombLocation, setBombLocation] = useState(Math.floor(Math.random() * board.length))
+
+  const [counter, setCounter] = useState(5);
+
+  const handleGamePlay = (clickedSquare) => {
+    // makes a copy of the board in state so that we can modify it in this method
+    let updateBoard = [...board]
+    if (clickedSquare === treasureLocation) {
+      updateBoard[clickedSquare] = "💎"
+      setBoard(updateBoard)
+    } else if (clickedSquare === bombLocation) {
+      updateBoard[clickedSquare] = "💣"
+      setBoard(updateBoard)
+    } else {
+      updateBoard[clickedSquare] = "🌲"
+      setBoard(updateBoard)
+      setCounter(counter - 1)
+    }
+  }
+
+  const handleReset = () => {
+    setBoard([
+      "?",
+      "?",
+      "?",
+      "?",
+      "?",
+      "?",
+      "?",
+      "?",
+      "?"
+    ])
+    setTreasureLocation(Math.floor(Math.random() * board.length))
+    setBombLocation(Math.floor(Math.random() * board.length))
+  }
+
+  console.log("treasureLocation:", treasureLocation)
+  console.log("bombLocation:", bombLocation)
+
+  return (
+    <>
+      <h1>Treasure Hunt Game</h1>
+      <div className="board-game">
+        {board.map((square, index) => {
+          return <Square
+            square={square}
+            index={index}
+            key={index}
+            handleGamePlay={handleGamePlay} />
+        })}
+      </div>
+      <br></br>
+      <div class="resetgame">
+        <button onClick={handleReset}>Restart Game</button>
+      </div>
+    </>
+  )
+}
+
+export default App;
